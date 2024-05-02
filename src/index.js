91f8d50553a2339f510ac72cf7a816872421af77
Anirudh Ganapathy <anirudhganapathy2003@gmail.com>
2024-05-02 06:10:05 +0530

    improve UI

--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
 import bodyParser from "body-parser";
 import express from "express";
 import morgan from "morgan";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-app.use(morgan("tiny"));
-app.use(bodyParser.urlencoded({extended: true})); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 const app = express();
+app.use("/static",express.static(__dirname + '/public'));
 
-const port=3000; 
 
-app.get("/", function(req, res) {
-    res.send("Hello there");
+app.use(morgan("tiny"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+
+const users = [
+    {
+        id: 1,
+        name: "Aaron",
+        age: 32,
+        blood: "B+ve",
+        allergies: ["Nuts", "Crabs"]
+    },
+];
+
+const port = 3000;
+
+
+
+app.get("/", function (req, res) {
+    res.render(__dirname + '/public/index', {user: users[0]});
 });
 
+app.get("/users/:id", function (req, res) {
+    const ID = parseInt(req.params['id']);
+    const user = users.find(user => user.id == ID); 
+    if (user) {
+        res.render(__dirname + "/public/index", {user: user});
+    }
+    else {
+        res.send("No user with the matching ID is found.");
+    }
+});
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log(`Server started on port ${port}`);
 });
